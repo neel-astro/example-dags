3473844603e19999b0881aae57420b645ef57ff0
Neel Dalsania <neel.dalsania@astronomer.io>
2025-04-22 12:29:47 +0530

    Update simple_sleep.py

--- dags/simple_sleep.py
+++ dags/simple_sleep.py
@@ -23,7 +23,7 @@ def simple_sleep():
     def sleep2() -> None:
         """Sleep for 20 seconds."""
         import time
-        print("test3")
+        print("test4")
 
         time.sleep(20)
         
