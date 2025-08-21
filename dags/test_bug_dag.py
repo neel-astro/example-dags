ef38888fa97a94a31473b6e2370ee8b0a3ce21a9
Neel Dalsania <neel.dalsania@astronomer.io>
2025-08-21 20:03:18 +0530

    Create test_bug_dag.py

---
+++ dags/test_bug_dag.py
@@ -0,0 +1,31 @@
+from airflow.decorators import dag, task
+from airflow.models.xcom import XCom
+
+@task
+def generate_data():
+    print("XCom.__name__ --->", XCom.__name__)
+    return [1, 2, 3]
+
+
+@task
+def filter_item_from_mapped_task(item):
+    print("filter_item_from_mapped_task item --->", item)
+    if item==1:
+        return None
+    return item
+
+
+@task
+def print_item(item):
+    print("print_item item --->", item)
+
+
+@dag(
+    dag_id='test_bug_dag',
+)
+def test_bug_dag():
+    data = generate_data()
+    filtered_data = filter_item_from_mapped_task.expand(item=data)
+    print_item.expand(item=filtered_data)
+
+test_bug_dag()
