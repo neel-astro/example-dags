b97bd16cae07f4a88adc0c140f49e2da3607e9f0
Neel Dalsania <neel.dalsania@astronomer.io>
2025-11-05 18:17:28 +0530

    Update variable_check.py

--- dags/variable_check.py
+++ dags/variable_check.py
@@ -1,12 +1,13 @@
 from airflow.sdk import DAG
 from airflow.decorators import task
+from airflow.utils import timezone
 
-# DAG definition
+# Define DAG
 with DAG(
     dag_id="airflow_var_check",
     description="Check Airflow Variable 'healthcheck' for expected value",
-    schedule="@hourly",        # or None for manual
-    start_date="2024-01-01",
+    schedule="@hourly",
+    start_date=timezone.datetime(2024, 1, 1),
     catchup=False,
     tags=["monitoring", "healthcheck"],
 ):
@@ -15,7 +16,6 @@ with DAG(
     def check_healthcheck_variable():
         from airflow.models import Variable
 
-        # Get variable from Airflow (Key Vault backend if configured)
         value = Variable.get("healthcheck", default_var=None)
         if value is None:
             raise ValueError("❌ Variable 'healthcheck' not found.")
