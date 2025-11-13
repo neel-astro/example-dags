aa104a7816de7138eb18efbabf24d783f72349a5
Neel Dalsania <neel.dalsania@astronomer.io>
2025-11-13 13:33:41 +0530

    Update simple_sleep.py

--- dags/simple_sleep.py
+++ dags/simple_sleep.py
@@ -25,82 +25,8 @@ def simple_sleep():
         print("Running short on time, please work!!!!")
 
         time.sleep(30)
-    
-    @task
-    def sleep2() -> None:
-        """Sleep for 20 seconds."""
-        import time
-        print("test5")
-
-        time.sleep(20)
-        
-    @task
-    def sleep3() -> None:
-        """Sleep for 20 seconds."""
-        import time
-        print("testing")
-
-        time.sleep(20)
-        
-    @task
-    def sleep4() -> None:
-        """Sleep for 20 seconds."""
-        import time
-
-        time.sleep(20)
-    
-    @task
-    def sleep5() -> None:
-        """Sleep for 20 seconds."""
-        import time
-
-        time.sleep(20)
-        
-    @task
-    def sleep6() -> None:
-        """Sleep for 20 seconds."""
-        import time
-
-        time.sleep(20)
-    
-    @task
-    def sleep7() -> None:
-        """Sleep for 20 seconds."""
-        import time
-
-        time.sleep(20)
-        
-    @task
-    def sleep8() -> None:
-        """Sleep for 20 seconds."""
-        import time
-
-        time.sleep(20)
-        
-    @task
-    def sleep9() -> None:
-        """Sleep for 20 seconds."""
-        import time
-
-        time.sleep(20)
-    
-    @task
-    def sleep10() -> None:
-        """Sleep for 20 seconds."""
-        import time
-
-        time.sleep(20)
 
     sleep1()
-    sleep2()
-    sleep3()
-    sleep4()
-    sleep5()
-    sleep6()
-    sleep7()
-    sleep8()
-    sleep9()
-    sleep10()
 
 
 # Instantiate the DAG
