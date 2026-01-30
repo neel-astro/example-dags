004d2e020e7c37c2d1216a4f9ad35ef43d2650a8
Neel Dalsania <neel.dalsania@astronomer.io>
2026-01-30 21:45:10 +0530

    Update simple_sleep.py

--- dags/simple_sleep.py
+++ dags/simple_sleep.py
@@ -1,31 +1,31 @@
-from airflow.decorators import dag, task
+# from airflow.decorators import dag, task
 
 
-# Define the basic parameters of the DAG, like schedule and start_date
-@dag(
-    start_date=datetime(2024, 1, 1),
-    schedule="@daily",
-    catchup=False,
-    doc_md=__doc__,
-    default_args={
-        "owner": "Neel", 
-        "retries": 0,
-        "email": ["airflow@example.com"],
-        "email_on_failure": True,
-    },
-    tags=["example3"],
-)
-def simple_sleep():
-    @task
-    def sleep1() -> None:
-        """Sleep for 20 seconds."""
-        import time
-        print("Running short on time, please work!!!!")
+# # Define the basic parameters of the DAG, like schedule and start_date
+# @dag(
+#     start_date=datetime(2024, 1, 1),
+#     schedule="@daily",
+#     catchup=False,
+#     doc_md=__doc__,
+#     default_args={
+#         "owner": "Neel", 
+#         "retries": 0,
+#         "email": ["airflow@example.com"],
+#         "email_on_failure": True,
+#     },
+#     tags=["example3"],
+# )
+# def simple_sleep():
+#     @task
+#     def sleep1() -> None:
+#         """Sleep for 20 seconds."""
+#         import time
+#         print("Running short on time, please work!!!!")
 
-        time.sleep(300)
+#         time.sleep(300)
 
-    sleep1()
+#     sleep1()
 
 
-# Instantiate the DAG
-simple_sleep()
+# # Instantiate the DAG
+# simple_sleep()
